ff61b27f9a82ece3e2e39995699e207794637cbf
Gus Rasch <gusrasch@gmail.com>
2025-02-03 15:07:22 -0500

    Initial Commit

---
+++ Mooded/MoodedApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoodedApp.swift
+//  Mooded
+//
+//  Created by Gus Rasch on 2/3/25.
+//
+
+import SwiftUI
+
+@main
+struct MoodedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MoodedTests/MoodedTests.swift
@@ -0,0 +1,17 @@
+//
+//  MoodedTests.swift
+//  MoodedTests
+//
+//  Created by Gus Rasch on 2/3/25.
+//
+
+import Testing
+@testable import Mooded
+
+struct MoodedTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
